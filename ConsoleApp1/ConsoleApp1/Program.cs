de074027a74ee961ad0e5b942ae1ec43e5994bbc
frocker <felixcrocker@hotmail.co.uk>
2018-04-24 21:49:50 +0100

    Final version
    Final push, some tweaks for final user studies

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,46 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using Tobii.Interaction;
 using Tobii.Interaction.Framework;
 using Tobii.Interaction.Model;
 
+
+
+
 namespace ConsoleApp1
 {
     class Program
     {
+
+        [DllImport("User32.dll")]
+        public static extern System.IntPtr FindWindowEx(System.IntPtr parent, System.IntPtr childe, string strclass, string strname);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        [DllImport("Kernel32.dll")]
+        public static extern System.IntPtr GetConsoleWindow();
+
         static void Main(string[] args)
         {
+
+            IntPtr p = GetConsoleWindow();
+            ShowWindow(p, 3); //0,1,2,3 - Close, normal, min, max
+
+            Console.WriteLine("*******************************************************************\n");
+            Console.WriteLine("                       Gaze Interactive Art                        \n");
+            Console.WriteLine("*******************************************************************\n\n");
+            Console.WriteLine("Press any key to begin");
+            Console.ReadKey();
             
 
+            try
+            {
+                var env = System.Environment.CurrentDirectory;
+                env = Path.GetFullPath(Path.Combine(env, @"..\"));
+           
             // Everything starts with initializing Host, which manages connection to the 
             // Tobii Engine and provides all the Tobii Core SDK functionality.
             // NOTE: Make sure that Tobii.EyeX.exe is running
@@ -22,21 +50,24 @@ namespace ConsoleApp1
             System.Threading.Thread.Sleep(1000);
             host.Context.LaunchConfigurationTool(ConfigurationTool.RetailCalibration, (data) => { });
             System.Threading.Thread.Sleep(10000);
+            Console.WriteLine("Calibration Complete, minimising window\n");
+            //Minimise our current window
+            ShowWindow(p, 2);
+            Console.WriteLine("Minimised, opening Electron:\n");
+            Console.WriteLine(env);
+            //Open Electron.exe
+            Process.Start(Path.Combine(env, @"electron\electron.exe"));
 
-
-
-
+            //int id = process.Id;
+            //Process tempProc = Process.GetProcessById(id);
+            Console.WriteLine("Electron Opened, Setting up server\n");
             //Setup Server
             UdpClient udpClient = new UdpClient();
             udpClient.Connect("127.0.0.1", 33333);
-
+            Console.WriteLine("Server setup, initiating gaze data flow\n");
             //Create stream. 
             var gazePointDataStream = host.Streams.CreateGazePointDataStream();
-
-
-            //LaunchCalibration
-            
-       
+ 
             // Get the gaze data
             gazePointDataStream.GazePoint((x, y, ts) => SendInput(udpClient,x, y, ts));
 
@@ -46,7 +77,12 @@ namespace ConsoleApp1
 
             // we will close the coonection to the Tobii Engine before exit.
             host.DisableConnection();
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: "+e+"\n");
+                Console.ReadKey();
+            }
         }
 
         static void SendInput(UdpClient client, double x, double y, double ts)
